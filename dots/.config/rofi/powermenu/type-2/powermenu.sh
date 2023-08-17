4a4d53658aea91d27855898491f8f6964058333a
Axenide <axenide@gmail.com>
2023-08-17 18:50:02 -0300

    Added lock after suspend (i forgor 💀)

--- dots/.config/rofi/powermenu/type-2/powermenu.sh
+++ dots/.config/rofi/powermenu/type-2/powermenu.sh
@@ -69,6 +69,7 @@ run_cmd() {
 		elif [[ $1 == '--suspend' ]]; then
 			mpc -q pause
 			amixer set Master mute
+      swaylock
 			systemctl suspend
 		elif [[ $1 == '--logout' ]]; then
 			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
