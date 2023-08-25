9bae523f2ed2fdf106c99d7a5163f599679934e7
Axenide <axenide@gmail.com>
2023-08-24 22:38:03 -0300

    DID I REALLY FORGET ADDING KITTY TO THIS THING?!?!?!

---
+++ dots/.config/kitty/kitty-slurp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# execute slurp with -f "%x %y %w %h" and set variables
+read -r X Y W H < <(slurp -f "%x %y %w %h" -b 00000080 -c CC241D)
+# execute kitty with override and disown
+hyprctl dispatch -- exec "[move $X $Y]kitty -o initial_window_width="$W" -o initial_window_height="$H" --class kitty-floating"
+
+exit
--- dots/.config/tofi/wall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~/Imágenes/Wallpapers
-wallpaper=$(ls -p | grep -v / | tofi "$@" --prompt-text "Wallpaper: " | cut -d '' -f 1 | tr -d '\n')
-
-swww img -t any --transition-bezier 0.0,0.0,1.0,1.0 --transition-duration .75 --transition-step 255 --transition-fps 60 "$wallpaper" && \
-ln -sf ~/Imágenes/Wallpapers/"$wallpaper" ~/Imágenes/Wallpapers/current.wall
-exit
