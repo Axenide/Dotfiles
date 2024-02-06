5416ffbfe2f89ec6360c01a889f5f87d3c554712
Axenide <axenide@gmail.com>
2024-02-06 03:43:21 -0300

    Fixed firefox.sh script

--- firefox.sh
+++ firefox.sh
@@ -10,8 +10,14 @@ firefox_folder="$HOME/.mozilla/firefox"
 default_release_dir=$(find "$firefox_folder" -type d -name "*.default-release")
 
 if [ -n "$default_release_dir" ]; then
+    # Borra la carpeta "chrome" si existe
+    rm -rf "$default_release_dir/chrome"
+
     # Crea symlink a la carpeta "chrome"
     ln -sf "$original_folder/chrome" "$default_release_dir/chrome"
+    
+    # Borra el archivo "user.js" si existe
+    rm -f "$default_release_dir/user.js"
 
     # Crea symlink al archivo "user.js"
     ln -sf "$original_folder/user.js" "$default_release_dir/user.js"
