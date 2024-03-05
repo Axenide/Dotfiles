9211159107720b8533a9a14e5e090faf9724878d
Axenide <axenide@gmail.com>
2024-03-04 23:32:50 -0300

    Added Zoxide

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -7,5 +7,5 @@ thefuck --alias | source
 starship init fish | source
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/env.fish
-
+zoxide init fish | source
 [ -f ~/.inshellisense/key-bindings.fish ] && source ~/.inshellisense/key-bindings.fish
