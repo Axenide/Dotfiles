540fa6555d92125990d5ace4436eafb3325cd5ca
Axenide <axenide@gmail.com>
2024-08-20 16:24:09 -0300

    Fixed NvChad

--- term/.config/nvim/init.lua
+++ term/.config/nvim/init.lua
@@ -14,7 +14,7 @@ vim.g.mapleader = " "
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
@@ -30,9 +30,6 @@ require("lazy").setup({
     lazy = false,
     branch = "v2.5",
     import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
   },
 
   { import = "plugins" },
@@ -42,8 +39,8 @@ require("lazy").setup({
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
+require "options"
 require "nvchad.autocmds"
-require("nvchad.utils").reload("themes.wal")
 
 vim.schedule(function()
   require "mappings"
